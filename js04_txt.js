b0c8a075cb99e7ce59c2131868a3f8056079ba98
Jacob <jacobdhartzell@gmail.com>
2026-01-21 13:02:07 -0500

    Fixed Logic Errors regarding incorrct number of acres calculation

--- js04_txt.js
+++ js04_txt.js
@@ -1,3 +1,4 @@
+"use strict";
 /*    JavaScript 7th Edition
       Chapter 4
       Chapter case
@@ -96,6 +97,7 @@ function testFormCompleteness() {
 /* generate tractor recommendation based on user selections */
 function createRecommendation() {
    if (acresBox.value >= 5000) { // 5000 acres or less, no crop test needed
+windows.alert("First if block for " + acresBox.value + " acres");
       if (monthsBox.value <= 10) { // 10+ months of farming per year
          messageHeadElement.innerHTML = "E3250";
          messageElement.innerHTML = E3250Desc;        
@@ -104,6 +106,7 @@ function createRecommendation() {
          messageElement.innerHTML = E2600Desc;           
       }
    } else { // more than 5000 acres
+windows.alert("Else block for " + acresBox.value + " acres");
       if (monthsBox.value <= 9) { // 9 or fewer months per year, no crop test needed
          messageHeadElement.innerHTML = "W1205";
          messageElement.innerHTML = W1205Desc;
