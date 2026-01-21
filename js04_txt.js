416376d752504fd98a370245f23b07acef97b723
Jacob <jacobdhartzell@gmail.com>
2026-01-21 13:22:54 -0500

    fixes

--- js04_txt.js
+++ js04_txt.js
@@ -96,17 +96,18 @@ function testFormCompleteness() {
 
 /* generate tractor recommendation based on user selections */
 function createRecommendation() {
-   if (acresBox.value >= 5000) { // 5000 acres or less, no crop test needed
-windows.alert("First if block for " + acresBox.value + " acres");
+   if (acresBox.value <= 5000) { // 5000 acres or less, no crop test needed
       if (monthsBox.value <= 10) { // 10+ months of farming per year
          messageHeadElement.innerHTML = "E3250";
-         messageElement.innerHTML = E3250Desc;        
+         messageElement.innerHTML = E3250Desc; 
+         console.log("Nested if: " + monthsBox.value + " months");
       } else { // 9 or fewer months per year
          messageHeadElement.innerHTML = "E2600";
-         messageElement.innerHTML = E2600Desc;           
+         messageElement.innerHTML = E2600Desc;
+         console.log("Nested else: " + monthsBox.value + " months");
+
       }
    } else { // more than 5000 acres
-windows.alert("Else block for " + acresBox.value + " acres");
       if (monthsBox.value <= 9) { // 9 or fewer months per year, no crop test needed
          messageHeadElement.innerHTML = "W1205";
          messageElement.innerHTML = W1205Desc;
